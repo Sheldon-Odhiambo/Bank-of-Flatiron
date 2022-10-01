842365b113322d31fce7edb12c709112eb017924
JUdihouma04 <sheldonodhiuma04@gmail.com>
2022-10-01 05:24:58 +0300

    deliverables uploaded

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, {  useState } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
+  const [transaction, setTransaction] =useState([])
+  const [search, setSearch] =useState("")
+  
   return (
     <div>
-      <Search />
-      <AddTransactionForm />
-      <TransactionsList />
+      <Search search={search} setSearch={setSearch} />
+      <AddTransactionForm transactions={transaction} setTransactions={setTransaction} />
+      <TransactionsList transactions={transaction}/>
     </div>
   );
 }
