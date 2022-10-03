3fb0568bf9b7d3a97db0e04ef7544c9fdc8044f5
JUdihouma04 <sheldonodhiuma04@gmail.com>
2022-10-03 14:52:17 +0300

    debugging

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,19 +1,27 @@
-import React, {  useState } from "react";
+import React, { useEffect, useState } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
-  const [transaction, setTransaction] =useState([])
-  const [search, setSearch] =useState("")
-  
+  const [transaction, setTransaction] = useState([])
+  const [query, setQuery] = useState("")
+  useEffect(() => {
+    fetch("http://localhost:8001/transactions?q=" + query)
+      .then((resp) => resp.json())
+      .then(transaction => setTransaction(transaction))
+  }, [query])
+  function handleSearch(e) {
+    setQuery(e.target.value)
+  }
   return (
     <div>
-      <Search search={search} setSearch={setSearch} />
-      <AddTransactionForm transactions={transaction} setTransactions={setTransaction} />
-      <TransactionsList transactions={transaction}/>
+      <Search handleSearch={handleSearch} />
+      <AddTransactionForm />
+      <TransactionsList transactions={transaction} />
     </div>
   );
+  
 }
 
 export default AccountContainer;
